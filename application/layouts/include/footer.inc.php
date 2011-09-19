596abb04760be5a36eafc8d052f303ccc737f243
gbby <gabby.resch@utoronto.ca>
2011-09-19 03:23:50 -0400

    Updated footer text...

--- application/layouts/include/footer.inc.php
+++ application/layouts/include/footer.inc.php
@@ -2,6 +2,6 @@
 // get languge array
 ?>
 <div id="global_footer">
-	<div>PLACE.web is developed by Encorlab at OISE, University of Toronto</div>
-	<div>(c) 2011, University of Toronto, Software released under a <a href="http://www.opensource.org/licenses/mit-license.php">MIT license</a></div>
+	<div>PLACE.web is developed by ENCORE lab at OISE, University of Toronto</div>
+	<div>© 2011, University of Toronto, Software released under an <a href="http://www.opensource.org/licenses/mit-license.php">MIT license</a></div>
 </div><!-- /global_footer -->

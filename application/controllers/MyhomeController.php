6a3dfb9107bed5792664feaa542f2fca5297e25d
Ali Ajellu <ali.ajellu@gmail.com>
2011-09-21 00:50:50 -0400

    calculate comment score

--- application/controllers/MyhomeController.php
+++ application/controllers/MyhomeController.php
@@ -10,10 +10,40 @@ class MyhomeController extends Zend_Controller_Action
 
     public function indexAction()
     {
-    	// calculate tagging and voting scores
+    	// calculate comment vote score
+    	$this->view->commentScore = $this->calculateCommentScore();
     	
+    	// calculate concept connection votes score
+    	$this->view->tagScore = $this->calculateTagScore();
     	
-    	
+    }
+    
+    public function calculateCommentScore(){
+        // get all user's comments
+        $comments = Doctrine::getTable("Comment")
+                    ->findByDql("author_id = ? AND run_id = ?", array($_SESSION['author_id'], $_SESSION['run_id']));
+        
+        $commentIds = array();
+        foreach ($comments as $comment){
+            $commentIds[] = $comment->id;
+        }
+        
+        if (count($comments) == 0){
+            return 0;
+        }
+        
+        // sum up all the votes for the user comments found
+        $votes = Doctrine_Query::create()
+                    ->select("sum(vote_value) as vote_sum")
+                    ->from("Vote")
+                    ->whereIn("obj_id", $commentIds)
+                    ->andWhere("obj_type = ?", Votable::$COMMENT)
+                    ->execute();
+                    
+        return $votes[0]['vote_sum'];
+    }
+    
+    public function calculateTagScore(){
         
     }
 

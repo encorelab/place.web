9a3c52ff6b6bafe65ae2327a43937a42edecd62f
Antonio Gamba Bari <lodeanto@gmail.com>
2011-11-01 12:00:14 -0500

    Added edit functionality for example and other CSS fixes

--- application/models/static/PlaceWebTools.php
+++ application/models/static/PlaceWebTools.php
@@ -24,7 +24,7 @@ class PlaceWebTools
 		{
 			if($val==$selectedVal)
 			{
-				$selected=" selected";
+				$selected=' selected="selected"';
 			} else {
 				$selected="";
 			}
@@ -63,5 +63,102 @@ class PlaceWebTools
 		return $html;
 		
 	} // end arrayToHtmlSelect()
+
+	public static function showImgVideoPlayer($dataArray)
+	{
+			$mediaFile = current($dataArray);
+			$html='';
+			// need the determine the media type here
+			
+			if (preg_match("/video/i", $mediaFile['media_type']))
+			{
+				$html.='
+			<div class="dashlet-box-image">
+	          <div class="dashlet-title">'. $dataArray[0]['name'].'</div>
+				<div id="jp_container_1" class="jp-video jp-video-270p">
+	                        <div class="jp-type-single">
+	                                <div id="jquery_jplayer_1" class="jp-jplayer"></div>
+	                                <div class="jp-gui">
+	                                        <div class="jp-video-play">
+	                                                <a href="javascript:;" class="jp-video-play-icon" tabindex="1">play</a>
+	                                        </div>
+	                                        <div class="jp-interface">
+	                                                <div class="jp-progress">
+	                                                        <div class="jp-seek-bar">
+	                                                                <div class="jp-play-bar"></div>
+	                                                        </div>
+	                                                </div>
+	                                                <div class="jp-current-time"></div>
+	                                                <div class="jp-duration"></div>
+	                                                <div class="jp-title">
+	                                                        <ul>
+	                                                                <li>' . $mediaFile['name'] . '</li>
+	                                                        </ul>
+	                                                </div>
+	                                                <div class="jp-controls-holder">
+	                                                        <ul class="jp-controls">
+	                                                                <li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
+	                                                                <li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
+	                                                                <li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
+	                                                                <li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
+	                                                                <li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
+	                                                                <li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
+	                                                        </ul>
+	                                                        <div class="jp-volume-bar">
+	                                                                <div class="jp-volume-bar-value"></div>
+	                                                        </div>
+	
+	                                                        <ul class="jp-toggles">
+	                                                                <li><a href="javascript:;" class="jp-full-screen" tabindex="1" title="full screen">full screen</a></li>
+	                                                                <li><a href="javascript:;" class="jp-restore-screen" tabindex="1" title="restore screen">restore screen</a></li>
+	                                                                <li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
+	                                                                <li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
+	                                                        </ul>
+	                                                </div>
+	                                        </div>
+	                                </div>
+	                                <div class="jp-no-solution">
+	                                        <span>Update Required</span>
+	                                        To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+	                                </div>
+	                        </div>
+	
+	                </div>
+		</div>
+			<script>
+				$("#jquery_jplayer_1").jPlayer({
+	                                        ready: function() { // The $.jPlayer.event.ready event
+	                                           $(this).jPlayer("setMedia", { m4v: "' . $mediaFile['media_content'] .  '" }); //.jPlayer("play");
+	                                        },
+	                                        ended: function() { // The $.jPlayer.event.ended event
+	                                            //$(this).jPlayer("play"); // Repeat the media
+	                                        },
+	                                        size: {
+	                                                            width: "460px",
+	                                                            height: "270px",
+	                                                            cssClass: "jp-video-270p"
+	                                                    },
+	
+	                                     solution:"html, flash",
+	                                     swfPath: "/jquery/jplayer",
+	                                     supplied: "m4v",
+	                                     errorAlerts: true
+	                                    });
+			</script>
+				';
+	
+				$html.= "<br/>";
+				
+			} else { 
+				
+	$html.= '
+	<div class="dashlet-box-image">
+	    <div class="dashlet-title">'. $dataArray[0]['name'].'</div>
+		<div class="image-holder"><img src="/content/'. $dataArray[0]['media_path'].'" title="'. $dataArray[0]['name'].'" width="320px" /></div>
+	</div>';
+			}
+			
+			return $html;
+	} // end fnc
 	
 } // end class

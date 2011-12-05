13c1777f66ebac3b537453feb46e2a9e16d3147b
Antonio Gamba Bari <lodeanto@gmail.com>
2011-12-05 16:59:36 -0500

    added answer stats to question view

--- application/models/Answer.php
+++ application/models/Answer.php
@@ -15,4 +15,51 @@ class Answer extends BaseAnswer
     public function getUrl(){
         return "/question/show?&id=".$this->id;
     }
+    
+    /**
+     * 
+     * Sum the number of each answer 
+     * @param array $answersArray
+     */
+    public static function generateAnswersGraph($answersArray)
+    {
+    	$html = "";
+    	$sum = array();
+    	$sum['A'] = 0;
+    	$sum['B'] = 0;
+    	$sum['C'] = 0;
+    	$sum['D'] = 0;
+    	$sum['E'] = 0;
+    	
+    	foreach ($answersArray as $answer)
+    	{
+    		if($answer['mc_chocie']=='A') {
+    			$sum['A'] += 1; 
+    		} else if($answer['mc_chocie']=='B'){
+    			$sum['B'] += 1;
+    		} else if($answer['mc_chocie']=='C'){
+    			$sum['C'] += 1;
+    		} else if($answer['mc_chocie']=='D'){
+    			$sum['D'] += 1;
+    		} else if($answer['mc_chocie']=='E'){
+    			$sum['E'] += 1;
+    		}
+    	}
+        	
+    	foreach ($sum as $name=>$val)
+   		{
+   			$html .= '
+		<div class="graph-item">
+   			<div class="answer-mc" style="float: none; margin-bottom: 3px;">'.$name.'</div>
+   			<div class="graph-bar" style="height: '.($val*10).'px;">'.$val.'</div>
+   		</div>
+   		';
+   		}
+   		
+   		$html .= '
+   		<div class="clear"></div>
+   		<div style="text-align: left; margin-left: 0.5em;">Total: <b>'.count($answersArray).'</b></div>
+   		';
+   		return $html;
+    }
 }

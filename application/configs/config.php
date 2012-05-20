dab7c369844a5a2d790efec59c1c049dca22e5b7
Antonio Gamba Bari <lodeanto@gmail.com>
2012-05-20 09:01:09 -0400

    added server config files and added path to NEOplace assets

--- application/configs/config.php
+++ application/configs/config.php
@@ -30,13 +30,12 @@ $PLACEWEB_CONFIG['ffmpegPath'] = '/usr/local/bin/ffmpeg';
 //$PLACEWEB_CONFIG['uploadDir'] = "/var/www/place/content/";
 
 // Antonio Config
-$PLACEWEB_CONFIG['uploadDir'] = "/var/www/mywebapps/PlaceWeb.GitHub/place.web.agamba/public/content/";
+//$PLACEWEB_CONFIG['uploadDir'] = "/var/www/mywebapps/PlaceWeb.GitHub/place.web.agamba/public/content/";
 
 // server config
-//$PLACEWEB_CONFIG['uploadDir'] = "/var/www/place/public/content/";
+$PLACEWEB_CONFIG['uploadDir'] = "/var/www/place/public/content/";
 
 $PLACEWEB_CONFIG['uploadWebDir'] = "/content/";
-
 
 //////////////////////////////////////////////////////////////////
 
@@ -72,7 +71,6 @@ $PLACEWEB_CONFIG['fConcepts'] = array(
 $PLACEWEB_CONFIG['questionTypes'] = array(
 	"LONG" => "Question-Narrative", // teacher 
 	"MC" => "Question-Multiple Choice", // teacher
-	"PROBLEM" => "Problem for Smart room" // teacher
 );
 
 // Node Types
@@ -82,4 +80,21 @@ $PLACEWEB_CONFIG['nodeTypes'] = array(
 	"3" => "Investigation", 
 	"4" => "Challenge Problem"
 );
+
+// Default Answer template
+$PLACEWEB_CONFIG['answerTemplate'] = "
+	<p><b>1. Provide a brief rationale for why you chose the answer you did.</b></p>
+	<p></p>
+	<p><b>2. How did examples provided with the question to help you understand and respond to the
+	problem?</b></p>
+	<p></p>
+	<p><b>3. How was each example connected or related to the challenge problem?</b></p>
+	<p></p>
+	<p><b>Example 1 was related as follows:</b></p>
+	<p></p>
+	<p><b>Example 2 was related as follows:</b></p>
+	<p></p>
+	<p><b>Example 3 was related as follows:</b></p>
+	<p></p>
+	<p><b>Example 4 was related as follows:</b></p>";
 ?>

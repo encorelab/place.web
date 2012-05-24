63557489d300aaa48b06af26860c99a769f47e96
Antonio Gamba Bari <lodeanto@gmail.com>
2012-05-23 22:07:24 -0400

    adding sql_config.php

---
+++ public/hwdata/sql_config.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * set SQL / Mongo credentials
+ */
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'anto123');
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_NAME', 'placeweb');
+
+?>
